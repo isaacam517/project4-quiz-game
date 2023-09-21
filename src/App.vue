b82b65554d27d10504dc9792457dc99df420b289
isaacam517 <isaacam517@gmail.com>
2023-09-21 16:27:54 -0300

    Primeiro Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,115 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount" />
+
+    <template v-if="this.question">
+
+      <h1 v-html="this.question"></h1>
+      
+      <template v-for="(answer, index) in this.answers" v-bind:key="index">
+        <input 
+          :disabled="this.answerSubmitted"
+          type="radio" 
+          name="options" 
+          :value="answer"
+          v-model="this.chosenAnswer"  
+        >
+          
+        <label v-html="answer"></label><br>
+      </template>
+        
+        <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Send</button>
+
+        <section v-if="this.answerSubmitted" class="result" >
+          
+            <h4 v-if="this.chosenAnswer == this.correctAnswer"
+              v-html="'&#9989; Parabéns, Miseravi!! a resposta '+ this.correctAnswer + ' está correta.'"
+            >
+              
+            </h4>
+
+            <h4 v-else
+            v-html="'&#10060; Errou, correta é ' + this.correctAnswer + '.'">
+             
+            </h4>
+         
+          <button class="send" @click="this.getNewQuestion()" type="button">Próxima pergunta</button>
+        </section>
+        
+    </template>
+      
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ScoreBoard from '@/components/ScoreBoard.vue'
 
-export default {
+  export default {
+   
+
   name: 'App',
   components: {
-    HelloWorld
+    ScoreBoard
+  },
+
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      winCount: 0,
+      loseCount: 0
+      
+    }
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+      return answers;
+    }
+  },
+  methods: {
+
+    submitAnswer() {
+      if (!this.chosenAnswer) {
+        alert('Escolha uma das opções');
+      } else {
+        this.answerSubmitted = true;
+        if (this.chosenAnswer == this.correctAnswer) {
+          this.winCount++;
+        } else {
+          this.loseCount++;
+        }
+      }
+    },
+    getNewQuestion() {
+
+      this.answerSubmitted = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+
+      this.axios
+      .get('https://opentdb.com/api.php?amount=1&category=18')
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.correctAnswer = response.data.results[0].correct_answer;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      })
+    }
+  },
+  created() {
+    this.getNewQuestion();
   }
+  
+  //https://opentdb.com/api.php?amount=1&category=18
 }
+
+
 </script>
 
 <style lang="scss">
@@ -21,6 +119,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//import store from './store'
+//import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAxios, axios)
+app.provide('axios', app.config.globalProperties.axios)  // provide 'axios'
+app.mount('#app')
